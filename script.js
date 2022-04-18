51888da68c9c5ca47c2d274166df44d48f8101ab
Ana Dejanović <ana.dejanovich@gmail.com>
2022-04-18 22:11:51 +0200

    Initial commit - pushing html, css and js code

--- script.js
+++ script.js
@@ -89,31 +89,6 @@ function getCardValue(value) {
   };
 
   return cardValueMap[value];
-  /*   switch (card.value) {
-    case "2":
-      return 2;
-    case "3":
-      return 3;
-    case "4":
-      return 4;
-    case "5":
-      return 5;
-    case "6":
-      return 6;
-    case "7":
-      return 7;
-    case "8":
-      return 8;
-    case "9":
-      return 9;
-    case "10":
-    case "J":
-    case "Q":
-    case "K":
-      return 10;
-    case "A":
-      return 11;
-  } */
 }
 
 // Calculate the dealer & player score
@@ -134,7 +109,6 @@ function updateScore() {
 // New game funcionality
 btnNewGame.addEventListener("click", function () {
   shuffledDeck = shuffleDeck([...orderedDeck]);
-  console.log(shuffledDeck.length);
   dealerScoreEl.textContent = 0;
   playerScoreEl.textContent = 0;
   resultText.textContent = "";
